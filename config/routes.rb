bc21e0ea2256d73f5d29f1e8993a108391383762
amcmax1 <95625051+amcmax1@users.noreply.github.com>
2022-04-26 02:11:10 +0300

    config(routes): routes modified to include endpoint to crate person record on post

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/persons', to: 'persons#index'
+      post '/persons', to: 'persons#create'
     end
   end
 end
