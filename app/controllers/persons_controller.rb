71afdb25028d5788d972f4eee76da2822248fb49
amcmax1 <95625051+amcmax1@users.noreply.github.com>
2022-04-26 02:01:24 +0300

    refactor(persons): persons model and controller modified to include new fetch articles service

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -3,7 +3,8 @@ module Api::V1
 
     def index
       @persons = Person.all
-      render json: @persons, include: ['articles']
+      render json: @persons, only: %i[id first_name second_name],
+                             include: { articles: { only: %i[id title url word_count source.name], include: { language: { only: %i[name value] } } } }
     end
 
   end
--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,16 @@ class Person < ApplicationRecord
   validates :country, presence: true
   has_many :articles, foreign_key: :person_id
   belongs_to :country, class_name: 'Country', foreign_key: :country_id
+
+  after_create :retrieve_articles
+
+  private
+
+  def retrieve_articles
+    active_sources = Source.where(status: 'active')
+
+    active_sources.each do |source|
+      FetchArticlesService.new(source: source, person: self).perform
+    end
+  end
 end
