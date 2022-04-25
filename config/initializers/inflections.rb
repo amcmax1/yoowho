c9c84abce9fa1acd0c9a411769fec25d10d51124
amcmax1 <95625051+amcmax1@users.noreply.github.com>
2022-04-25 23:07:00 +0300

    feat(inflections): inflections support for Person model namespace

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -11,6 +11,12 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
++ActiveSupport::Inflector.inflections(:en) do |inflect|
+  #   inflect.plural /^(ox)$/i, '\1en'
+  #   inflect.singular /^(ox)en/i, '\1'
+  inflect.irregular 'person', 'persons'
+  #   inflect.uncountable %w( fish sheep )
+end
+
+# These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
