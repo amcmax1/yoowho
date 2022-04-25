58474adc0c07670738367b294e1d63764928b1b1
amcmax1 <95625051+amcmax1@users.noreply.github.com>
2022-04-26 02:02:25 +0300

    fix(persons): added article destroy dependency

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ApplicationRecord
   validates :first_name, presence: true, uniqueness: true
   validates :second_name, uniqueness: true
   validates :country, presence: true
-  has_many :articles, foreign_key: :person_id
+  has_many :articles, foreign_key: :person_id, dependent: :destroy
   belongs_to :country, class_name: 'Country', foreign_key: :country_id
 
   after_create :retrieve_articles
