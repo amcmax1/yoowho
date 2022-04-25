ec30ee264ea35d83be9dd2e1eecbed62ee8425b8
amcmax1 <95625051+amcmax1@users.noreply.github.com>
2022-04-26 02:14:27 +0300

    feat(create Persons): WIP data obtained by request

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -7,5 +7,27 @@ module Api::V1
                              include: { articles: { only: %i[id title url word_count source.name], include: { language: { only: %i[name value] } } } }
     end
 
+    def create
+      country = Country.find_by(code: person_params[:country])
+
+      @persons = Person.create(
+        first_name: person_params[:first_name],
+        second_name: person_params[:second_name],
+        country: country)
+
+      if @persons.save!
+        render json: @person, status: :created
+      else
+        render json: @person.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def person_params
+      params
+          .require(:person)
+          .permit(:first_name, :second_name, :country)
+    end
   end
 end
